0ed77e0fceb16c7d2f849b5d741fd489f0c644f7
Pablo Miguel <pmigueld@iesch.org>
2024-12-11 15:29:47 +0100

    Repaso 2

---
+++ UD4_JAVASCRIPT/RepasoExamenCSS-JS/js/repaso.js
@@ -0,0 +1,26 @@
+let nombre = "Pablo"
+let edad = 17
+let ciudad = "Teruel"
+
+// EJ
+// EJ
+// EJ
+// EJ
+// EJ
+// EJ
+// EJ
+// EJ
+// EJ
+// EJ
+// EJ
+// EJ
+// EJ
+// EJ
+// EJ
+// EJ
+// EJ
+// EJ
+// EJ
+// EJ
+// EJ
+
