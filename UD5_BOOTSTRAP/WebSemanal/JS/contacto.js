19759006614102b8de32507f83b93f9cc9fb361d
ImNotPablosKi <pmigueld@iesch.org>
2025-01-22 14:17:39 +0100

    Galería: JS

--- UD5_BOOTSTRAP/WebSemanal/JS/contacto.js
+++ UD5_BOOTSTRAP/WebSemanal/JS/contacto.js
@@ -1,3 +1,29 @@
 function exito() {
-    
+    let cosilla = document.getElementById("buenaEsa");
+
+    if (cosilla.classList.contains("d-none")) {
+        cosilla.classList.remove("d-none");
+    }
+}
+
+function nuh() {
+
+    let cosilla = document.getElementById("malaEsa");
+
+    if (cosilla.classList.contains("d-none")) {
+        cosilla.classList.remove("d-none");
+    }
+
+}
+
+function cerrarMal() {
+    let cosilla = document.getElementById("malaEsa");
+
+    cosilla.classList.add("d-none");
+}
+
+function cerrarBien() {
+    let cosilla = document.getElementById("buenaEsa");
+
+    cosilla.classList.add("d-none");
 }
---
+++ UD5_BOOTSTRAP/WebSemanal/JS/galeria.js
@@ -0,0 +1,4 @@
+function imagen(ruta) {
+    document.getElementById('img-BIG').src = ruta;
+    
+}
